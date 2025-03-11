0a644c48f9e8772320e83e97c86a20f771672cf2
Clivon Osire <clivon254@icloud.com>
2025-03-11 12:29:56 +0300

    thum mora

--- server/controller/clientController.js
+++ server/controller/clientController.js
@@ -1,4 +1,8 @@
-import { errorHandler } from "../utils/error"
+
+import Client from "../model/clientController.js"
+import { errorHandler } from "../utils/error.js"
+
+
 
 
 export const addClient = async (req,res,next) => {
@@ -7,11 +11,21 @@ export const addClient = async (req,res,next) => {
 
     if(!req.user.isAdmin)
     {
-        return next(errorHandler(401,"You are not ad"))
+        return next(errorHandler(401,"You are not add client"))
     }
 
     try
-    {}
+    {
+
+        const newClient = new Client({
+            name,email,phone,additional,whatsapp
+        })
+
+        await newClient.save()
+
+        res.status(200).json({success:true , newClient})
+
+    }
     catch(error)
     {
         next(error)
@@ -22,8 +36,21 @@ export const addClient = async (req,res,next) => {
 
 export const getClient = async (req,res,next) => {
 
+    const {clientId} = req.params 
+
     try
-    {}
+    {
+
+        const client = await Client.findById(clientId)
+
+        if(!client)
+        {
+            return next(errorHandler(404, "client not found"))
+        }
+
+        res.status(200).json({success:true , client})
+
+    }
     catch(error)
     {
         next(error)
@@ -34,8 +61,18 @@ export const getClient = async (req,res,next) => {
 
 export const getClients = async (req,res,next) => {
 
+    if(!req.user.isAdmin)
+    {
+        return next(errorHandler(401,"You are not allowed to access the clients"))
+    }
+
     try
-    {}
+    {
+        const clients = await Client.find().sort({_id:-1})
+
+        res.status(200).json({success:true , clients})
+
+    }
     catch(error)
     {
         next(error)
@@ -46,8 +83,40 @@ export const getClients = async (req,res,next) => {
 
 export const updateClient = async (req,res,next) => {
 
+    if(!req.user.isAdmin)
+    {
+        return next(errorHandler(401,"You are not allowed to update the clients"))
+    }
+
+    const {clientId} = req.params
+
+    const client = await Client.findById(clientId)
+
+    if(!client)
+    {
+        return next(errorHandler(404,"client not found"))
+    }
+
     try
-    {}
+    {
+
+        const updatedClient = await Client.findByIdAndUpdate(
+            clientId ,
+            {
+                $set:{
+                    name:req.body.name ,
+                    email:req.body.email ,
+                    phone:req.body.phone ,
+                    whatsapp:req.body.whatsapp ,
+                    additional:req.body.additional 
+                }
+            },
+            {new:true}
+        )
+
+        res.status(200).json({success:true , updatedClient})
+
+    }
     catch(error)
     {
         next(error)
@@ -57,9 +126,29 @@ export const updateClient = async (req,res,next) => {
 
 
 export const deleteClient = async (req,res,next) => {
+    
+    if(!req.user.isAdmin) 
+    {
+        return next(errorHandler(401,"You are not allowed to update the clients"))
+    }
+
+    const {clientId} = req.params
+
+    const client = await Client.findById(clientId)
+
+    if(!client)
+    {
+        return next(errorHandler(404,"client not found"))
+    }
 
     try
-    {}
+    {
+
+        await Client.findByIdAndDelete(clientId)
+
+        res.status(200).json({success:true , message:`${client.name} is deleted successfully`})
+
+    }
     catch(error)
     {
         next(error)
--- server/router/clientRouter.js
+++ server/router/clientRouter.js
@@ -1,8 +1,27 @@
 
 import express from "express"
+import { verifyToken } from "../utils/verify.js"
+import { addClient, deleteClient, getClient, getClients, updateClient } from "../controller/clientController.js"
+
 
 
 const clientRouter = express.Router()
+
+
+
+clientRouter.post('/add-client', verifyToken ,addClient)
+
+
+clientRouter.get('/get-client/:clientId', getClient)
+
+
+clientRouter.get('/get-clients', getClients)
+
+
+clientRouter.put('/update-client/:clientId', verifyToken , updateClient)
+
+
+clientRouter.delete('/delete-client/:clientId', verifyToken , deleteClient)
 
 
 
