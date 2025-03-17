0eb4dfa304ba60ccb694330f3dd977a9cedfce3f
Clivon Osire <clivon254@icloud.com>
2025-03-17 18:59:39 +0300

    redux

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,8 +5,10 @@ export default function App() {
 
   return (
 
-    <div className="text-3xl text-red-400 flex items-center justify-center min-h-screen">
-      ADMIN SIRE TECHNOLOGY 
+    <div className="text-3xl text-red-400 flex flex-col items-center justify-center min-h-screen">
+     
+      <h2 className="">ADMIN SIRE TECHNOLOGY </h2>
+
     </div>
 
   )
---
+++ admin/src/redux/user/userSlice.js
@@ -0,0 +1,102 @@
+
+
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const initialState = {
+    currentUser:null,
+    error:null,
+    loading:false
+}
+
+
+const userSlice = createSlice({
+    name:'user',
+    initialState,
+    reducers:{
+
+        signInStart:(state,action) => {
+
+            state.loading = true
+
+            state.error = null
+
+        },
+
+        signInSuccess:(state,action) => {
+
+            state.loading = false 
+
+            state.error = null
+
+            state.currentUser = action.payload
+
+        },
+
+        signInFailure:(state,action) => {
+
+            state.loading = false
+
+            state.error = action.payload
+
+        },
+
+        updateUserStart:(state,action) => {
+
+            state.loading = true ,
+
+            state.error = null
+
+        },
+
+        updateUserSuccess:(state,action) => {
+
+            state.loading = false
+
+            state.error = null 
+
+            state.currentUser = action.payload 
+        },
+
+        updateUserFailure:(state,action) => {
+
+            state.loading = false
+
+            state.error = action.payload
+
+        }, 
+
+        deleteUserSuccess:(state,action) => {
+
+            state.currentUser = null
+
+            state.error = null
+        }, 
+
+        deleteUserFailure:(state,action) => {
+
+            state.loading = false
+
+            state.error = action.payload
+
+        },
+
+        signOutSuccess:() => {
+
+            state.currentUser = null
+
+        }
+
+
+    }
+})
+
+export const {
+    signInStart,signInSuccess,signInFailure,
+    updateUserStart,updateUserSuccess,updateUserFailure,
+    deleteUserSuccess,deleteUserFailure,
+    signOutSuccess
+} = userSlice.actions
+
+
+export default userSlice.reducer
