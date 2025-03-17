c75827f5705d9ecd76f4ed18fddfef25a09ec72e
Clivon Osire <clivon254@icloud.com>
2025-03-17 18:02:58 +0300

    shut

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,12 +1,36 @@
 
+
+const flowbite = require("flowbite-react/tailwind")
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content()
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+        'primary': '#C70000',
+        'secondary':"#000",
+        'blue':"#121fba",
+        'green':"#12ba51",
+        "purple":"#8F05F1"
+      },
+
+      fontFamily:{
+
+        text:["Poppins", "serif"],
+
+        title:["Titillium Web", "serif"],
+
+      }
+
+    },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
