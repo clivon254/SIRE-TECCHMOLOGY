6142801d6571625bf9d8b02a2a279d35d8077626
Clivon Osire <clivon254@icloud.com>
2025-03-18 07:09:42 +0300

    tuesday march

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,70 @@
 
 import React from 'react'
+import {BrowserRouter,Routes,Route,Outlet, Navigate} from "react-router-dom"
+import {Toaster} from "sonner"
+import {useSelector} from "react-redux"
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import ResetPassword from './pages/ResetPassword'
+
+
+
+
+const LayOut = () => {
+
+  const {currentUser} = useSelector(state => state.user)
+
+  return (
+
+    currentUser?.isAdmin ? 
+
+    <div className="">
+      
+      <Outlet />
+
+    </div>
+
+    :
+
+    <Navigate to="/sign-in"/>
+
+  )
+
+}
+
 
 export default function App() {
 
   return (
 
-    <div className="text-3xl text-red-400 flex flex-col items-center justify-center min-h-screen">
-     
-      <h2 className="">ADMIN SIRE TECHNOLOGY </h2>
+    <BrowserRouter>
+    
 
-    </div>
+      <main className="w-full  min-h-screen">
+
+          <Routes>
+
+            <Route element={<LayOut/>}>
+
+                <Route path="/" element={<Dashboard/>}/>
+
+            </Route>
+
+            <Route path="/sign-in" element={<SignIn/>}/>
+
+            <Route path="/sign-up" element={<SignUp/>}/>
+
+            <Route path="/forgot-password" element={<ForgotPassword/>}/>
+
+            <Route path="/reset-password/:token" element={<ResetPassword/>}/>
+
+          </Routes>
+        
+      </main>
+    
+    </BrowserRouter>
 
   )
   
--- admin/src/pages/SignIn.jsx
+++ admin/src/pages/SignIn.jsx
@@ -1,12 +1,76 @@
 
 
-import React from 'react'
+import React, { useState } from 'react'
+import LOGOO from "../assets/SIRELOGO.png"
+
+
+
 
 export default function SignIn() {
 
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
 
-    <div>SignIn</div>
+    <div className="section flex  items-center justify-between min-h-screen">
+
+      <div className="w-full  gap-y-5 ">
+
+        {/* header */}
+        <div className="w-full flex flex-col items-center">
+
+          {/* logo */}
+          <div className="h-24 w-60">
+
+            <img 
+              src={LOGOO}
+              alt=""
+              className="" 
+            />
+
+          </div>
+
+          {/* title */}
+          <h2 className="text-center title2">Sign in to your account</h2>
+
+        </div>
+
+        {/* form */}
+        <form action="" className="w-full mx-auto max-w-lg">
+
+          {/* email */}
+          <div className=" flex flex-col gap-y-2">
+
+              <label className="label">password</label>
+
+
+              <input type="text" className="input" />
+
+          </div>
+
+          {/* password */}
+          <div className=" flex flex-col gap-y-2">
+
+              <label className="label">password</label>
+
+              <div className="w-full relative">
+
+                  <input 
+                    type={showPassword ? "text" :"password"} 
+                    className="input" 
+                  />
+
+                  <button className="button"></button>
+
+              </div>
+
+          </div>
+
+        </form>
+
+      </div>
+
+    </div>
 
   )
   
